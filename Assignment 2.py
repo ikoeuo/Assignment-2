1afec1ff1cdb694915d63b47c0a245f9d8d106bd
nick <146023067+ikoeuo@users.noreply.github.com>
2023-10-26 14:30:35 -0400

    base

---
+++ Assignment 2.py
@@ -0,0 +1,22 @@
+import random
+
+class Product:
+    def __init__(self, code, name, saleP, costM, stockL, aproxMonthlyUnits):
+        self.code = code
+        self.name = name
+        self.saleP = saleP
+        self.costM = costM
+        self.stockL = stockL\
+        self.aproxMonthlyUnits = aproxMonthlyUnits
+
+    def simulateMonthlySales(self):
+
+
+    def stock(self):
+
+
+
+
+
+
+        
