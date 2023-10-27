93816a42e48c3c71f713f0becd88795193cee469
nick <146023067+ikoeuo@users.noreply.github.com>
2023-10-27 06:49:14 -0400

    cant get part of it to work :(

---
+++ Assignment2 App.py
@@ -0,0 +1,24 @@
+from Assignment2 import Product 
+
+
+def main():
+    code = int(input("Enter Product Code (100-10000): \n"))
+    name = input("Enter Product Name: \n")
+    saleP = float(input("Enter Product Sale Price, must be greater than 0: \n"))
+    costM = float(input("Enter Product Manufacture Cost, must be greater than 0: \n"))
+    stockL = int(input("Enter Stock Level, must be greater than 0: \n"))
+    monthlyUnitsM = int(input("Enter Monthly Units Manufactured, must be greater than or equal to 0: \n"))
+
+    if 100 <= code <= 10000 and saleP > 0 and costM > 0 and stockL > 0 and monthlyUnitsM >= 0:
+        product1 = Product(code, name, saleP, costM, stockL, monthlyUnitsM)
+        months = 12  
+
+        stockStatement = product1.stockS(months)
+        print(f"\nPredicted Stock Statement for the Next {months} Months: ")
+        print(stockStatement)
+
+    else:
+        print("Invalid input. Please make sure all input values are within the specified ranges.")
+
+
+main()
---
+++ Assignment2.py
@@ -0,0 +1,38 @@
+import random
+
+class Product:
+    def __init__(self, code, name, saleP, costM, stockL, monthlyUnitsM):
+        self.code = code
+        self.name = name
+        self.saleP = saleP
+        self.costM = costM
+        self.stockL = stockL
+        self.monthlyUnitsM = monthlyUnitsM 
+
+    def simulateMonthlySales(self):
+        unitsM = random.randint(self.monthlyUnitsM - 10, self.monthlyUnitsM + 10 )
+        unitsS = random.randint(0,unitsM)
+        self.stockL +=  self.monthlyUnitsM - unitsS
+
+    def stockS(self, months):
+        stockStatement = f"Product Code: {self.code}\nProduct Name: {self.name}\n"
+        stockStatement += f"Sale Price: {self.saleP}\nManufacture Cost: {self.costM}\n"
+
+        totalUnitsS = 0
+        totalUnitsM = 0
+        profit = 0
+ 
+        for i in range(1, months + 1):
+            unitsS = self.simulateMonthlySales()
+            totalUnitsS += unitsS
+            totalUnitsM += self.monthlyUnitsM
+            profit += (unitsS * self.saleP) - (self.monthlyUnitsM * self.costM)
+            stockStatement = f"\nMonth {i}: Stock Level = {self.stockL}, Units Sold = {unitsS}\n"
+            
+        stockStatement += f"Total Units Sold: {totalUnitsS}\n"
+        stockStatement += f"Total Units Manufactured: {totalUnitsM}\n"
+        stockStatement += f"Net Profit/Loss: {profit}\n"
+
+        return stockStatement 
+    
+  
